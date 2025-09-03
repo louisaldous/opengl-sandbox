9311688b7b609479e804fa4b92c371a34ac0a6b7
Louis Aldous <louisaldous01@gmail.com>
2025-09-03 19:59:55 +0100

    feat: Updated GLAD and added anisotropic filtering

--- src/main.cpp
+++ src/main.cpp
@@ -81,6 +81,7 @@ static uint32_t LoadTexture( std::string const &path, int textureUnit )
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, 16.0f );
 
     int width, height, channels;
     unsigned char *data = stbi_load( path.c_str(), &width, &height, &channels, 0 );
